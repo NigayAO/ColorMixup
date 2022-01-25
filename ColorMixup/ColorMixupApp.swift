7e39e279528a1c31247e0efb4b6f918daf2d6968
Alik <Nigay_AO@bk.ru>
2022-01-25 21:49:40 +0300

    Initial Commit

---
+++ ColorMixup/ColorMixupApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixupApp.swift
+//  ColorMixup
+//
+//  Created by Alik Nigay on 25.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorMixupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
